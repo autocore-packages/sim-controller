508f9f224b18ff1c77b53c64dd0582f7de0b2418
包涵 <1004578954@qq.com>
2020-11-04 18:28:29 +0800

    logicobj text rotate with camera

---
+++ Assets/sim-controller/Scripts/Runtime/FollowCameraRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.simController
+{
+
+    public class FollowCameraRotate : MonoBehaviour
+    {
+        public Vector3 offset;
+        // Update is called once per frame
+        void Update()
+        {
+            transform.rotation = Quaternion.Euler(OverLookCameraController.Instance.transform.rotation.eulerAngles + offset);
+        }
+    }
+}
